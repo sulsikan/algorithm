07d5abfb42956954e5fbd6f14be413ecbf2f36b7
주언 <137984851+sulsikan@users.noreply.github.com>
2025-05-23 22:24:25 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,14 @@
+def solution(my_string):
+    tokens = my_string.split()
+    result = int(tokens[0])
+    
+    for i in range(1, len(tokens), 2):
+        op = tokens[i]
+        num = int(tokens[i+1])
+        
+        if op == '+':
+            result += num
+        else:
+            result -= num
+            
+    return result
