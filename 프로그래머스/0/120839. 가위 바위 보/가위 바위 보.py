7a5596734c2c86836036d0464703b805e8bccfee
주언 <137984851+sulsikan@users.noreply.github.com>
2025-05-24 09:52:17 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,20 +1,6 @@
 '''
-2 -> 0
-0 -> 5
-5 -> 2
-
-205 -> replace 052
+다른 풀이 : 딕셔너리로 매핑
 '''
 def solution(rsp):
-    answer = ''
-    tokens = [int(char) for char in rsp]
-    
-    for i in range(len(tokens)):
-        if tokens[i] == 2:
-            tokens[i] = 0
-        elif tokens[i] == 0:
-            tokens[i] = 5
-        else:
-            tokens[i] = 2
-    answer = ''.join(str(digit) for digit in tokens)
-    return answer
+    d = {'0':'5','2':'0','5':'2'}
+    return ''.join(d[i] for i in rsp)
