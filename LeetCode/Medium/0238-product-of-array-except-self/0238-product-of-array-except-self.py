4d0a2b39724abf641bb4caf3ceef8a7a6167db88
주언 <137984851+sulsikan@users.noreply.github.com>
2025-11-06 16:37:16 +0900

    Time: 19 ms (87.14%), Space: 23.3 MB (55.38%) - LeetHub

---
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # 왼쪽 곱셈
+        out = []
+        p = 1
+        for i in range(0, len(nums)):
+            out.append(p)
+            p=p*nums[i]
+
+        p=1
+        # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+        for i in range(len(nums)-1,0 -1, -1):
+            out[i] = out[i] *p
+            p*=nums[i]
+        return out
