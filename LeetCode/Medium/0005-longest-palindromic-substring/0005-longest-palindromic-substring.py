2812e3bcf91fefaa79280d1e25550de9e26cc658
주언 <137984851+sulsikan@users.noreply.github.com>
2025-11-13 19:20:20 +0900

    Time: 43 ms (97.89%), Space: 17.8 MB (83.4%) - LeetHub

--- LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,18 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # 투포인터 확장으로 팰린드롬 판별
-        def expand(left:int, right:int) -> str:
+        # 251113
+        def lookup(left:int, right:int) -> str:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
             return s[left+1:right]
-
-        if len(s) < 2 or s == s[::-1]:
-            return s
-
-        result = ''
-        # 슬라이딩 윈도우 우측으로 이동
+        if len(s) <= 1 or s == s[::-1]: return s
+        answer = ''
         for i in range(len(s)-1):
-            result = max(result, expand(i, i+1), expand(i,i+2), key=len)
-        return result
-        
+            answer = max(answer, lookup(i, i+1), lookup(i, i+2), key=len)
+        return answer
