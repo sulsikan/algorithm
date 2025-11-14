24dd05c08a490cf8240116d52380a9d7d003d9f1
주언 <137984851+sulsikan@users.noreply.github.com>
2025-11-14 11:58:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32472 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,55 +1,32 @@
-'''
-1. 아이디어
-- 이중 for문 > 원소 1 && 방문한적 없음 > DFS실행, 단지 수++ > DFS결과 집 수 목록에 저장
-
-2. 시간복잡도
-DFS = O(V+E)
-V + E = N^2 + 4N^2 = 5N^2
---> O(5N^2) = O(N^2) <= 625
-2억 미만이니 가능
-
-3. 자료구조
-단지 수 int
-지도 int[][]
-방문확인 bool[][]
-단지별 집 수 int[]
-'''
-
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-map = [list(map(int, input().strip())) for _ in range(N)]
-chk = [[False] * N for _ in range(N)]
-result = []
-# 전역변수
+map = [list(map(int, input().strip())) for _ in  range(N)]
+#chk = [[False] * N for _ in range(N)]
 each = 0
 
-dy = [0,1,0,-1]
-dx = [1,0,-1,0]
-
 def dfs(y, x):
-    global each     # each는 전역변수기 때문에 함수 안에서 사용하려면 전역벽수로 바꿔주는 과정 필요함
+    # 종료 조건
+    if y < 0 or x < 0 or y >= N or x >= N or map[y][x]!=1: return
+    global each
     each += 1
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if 0 <= ny < N and 0 <= nx < N:
-            if map[ny][nx] == 1 and chk[ny][nx] == False:
-                chk[ny][nx] = True
-                dfs(ny, nx)
+    # 동서남북 탐색
+    map[y][x] = 0
+    dfs(y+1, x)
+    dfs(y - 1, x)
+    dfs(y, x+1)
+    dfs(y, x-1)
 
+# 탐색 시작
+ans = []
 for j in range(N):
     for i in range(N):
-        if map[j][i] == 1 and chk[j][i] == False:
-            # 방문 체크 표시
-            chk[j][i] = True
-            # DFS로 크기 구하기(함수에서 리턴값을 받아오는 것보다 전역변수를 사용하는 것이 편함)
+        if map[j][i] == 1:
             each = 0
             dfs(j, i)
-            result.append(each)
-
-result.sort()
-print(len(result))
-for i in result:
+            ans.append(each)
+ans.sort()
+print(len(ans))
+for i in ans:
     print(i)
