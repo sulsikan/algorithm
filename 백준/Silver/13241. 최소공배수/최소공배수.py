2564eb9aa3e4dd41859f56a974447e0061926158
주언 <137984851+sulsikan@users.noreply.github.com>
2025-11-15 21:31:55 +0900

    [Silver V] Title: 최소공배수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+a, b = map(int, input().split())
+
+def gcd(x, y):
+    if y == 0:
+        return x
+    return gcd(y, x % y)
+
+def lcm(x, y):
+    return x * y // gcd(x, y)
+
+print(lcm(a, b))
