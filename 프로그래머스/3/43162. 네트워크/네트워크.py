a111a10bfd832e979b9c2e6d37391d1745d222ad
주언 <137984851+sulsikan@users.noreply.github.com>
2025-11-16 01:51:14 +0900

    [level 3] Title: 네트워크, Time: 2.35 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,23 @@
+from collections import deque
 # 노드간선그래프-> dfs/bfs -> 다른 하나라도 이어져있으면 T 아니면 F
 def solution(n, computers):
     answer = 0
     visited = [False for _ in range(n)]
     for i in range(n):
         if visited[i] == False:
-            DFS(n, computers, i, visited)
+            BFS(n, computers, i, visited)
             answer += 1
     return answer
 
-def DFS(n, computers, i, visited):
-    visited[i] = True
-    for connect in range(n):
-        if computers[i][connect] == 1 and visited[connect] == False:
-            DFS(n, computers, connect, visited)
+def BFS(n, computers, i, visited):
+    queue = deque()
+    queue.append(i)
+    while queue:
+        com = queue.popleft()
+        visited[com] = True
+        for connect in range(n):
+            if visited[connect]==False and computers[com][connect] == 1:
+                queue.append(connect)
+                
+        
+            
