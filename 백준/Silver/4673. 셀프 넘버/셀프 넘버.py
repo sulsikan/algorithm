207943419a4d6b964c1ed17fab0142b2bdf24f7b
주언 <137984851+sulsikan@users.noreply.github.com>
2025-11-26 17:51:20 +0900

    [Silver V] Title: 셀프 넘버, Time: 40 ms, Memory: 33948 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,14 +1,9 @@
-def self_number(x):
-    num = list(str(x))
-    result = 0
-    for n in num:
-        result += int(n)
-    result += x
-    return result
+def d(n):
+    return n + sum(map(int, str(n)))
 
 results = set()
-for i in range(10001):
-    results.add(self_number(i))
-for j in range(10001):
+for i in range(1, 10001):
+    results.add(d(i))
+for j in range(1, 10001):
     if j not in results:
         print(j)
