57765fbfa6d47b4160d4822dfee74cb2049df17b
주언 <137984851+sulsikan@users.noreply.github.com>
2025-11-26 16:40:30 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 19648 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,73 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int [] stack;
+    public static int size =0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        stack = new int[n];
+        // 결과 값 출력위해 StringBuilder 생성(StringBuilder = String의 가변버전)
+        StringBuilder sb = new StringBuilder();
+
+        while(n-- > 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            switch(st.nextToken()){
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+
+                case "pop":
+                    sb.append(pop()).append('\n');
+                    break;
+
+                case "size":
+                    sb.append(size()).append('\n');  
+                    break;
+
+                case "empty":
+                    sb.append(empty()).append('\n');
+                    break;
+
+                case "top":
+                    sb.append(top()).append('\n');
+                    break;
+            } 
+        }
+        System.out.println(sb);
+    }
+    public static void push(int num){
+        stack[size] = num;
+        size++;
+    }
+    public static int pop(){
+        if(size==0){
+            return -1;
+        }else{
+            int res = stack[size-1];
+            stack[size-1]=0;
+            size--;
+            return res;
+        }
+    }
+    public static int size(){
+        return size;
+    }
+    public static int empty(){
+        if(size==0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    public static int top(){
+        if(size==0){
+            return -1;
+        }else{
+            return stack[size-1];
+        }
+    }
+}
