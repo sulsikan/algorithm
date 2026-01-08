b1f114487fcc8aa93b570beddc8d41acf9b72fcb
주언 <137984851+sulsikan@users.noreply.github.com>
2026-01-08 19:20:38 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,4 +1,5 @@
 -- IS NOT NULL
-SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d')as DATE_OF_BIRTH from MEMBER_PROFILE
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as date_of_birth
+from MEMBER_PROFILE
 where month(DATE_OF_BIRTH) = 03 and GENDER='W' and TLNO is not null
 order by member_id asc;
