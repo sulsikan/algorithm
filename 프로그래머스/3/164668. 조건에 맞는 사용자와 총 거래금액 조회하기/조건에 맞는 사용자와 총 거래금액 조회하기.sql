eb20bd9269014e5f1edb8ad12f12fbfbef5ff301
주언 <137984851+sulsikan@users.noreply.github.com>
2026-01-09 16:26:47 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,15 +1,28 @@
+# -- 완료된 중고거래의 총금액이 70만원 이상인 사람
+# -- STATUS = 'DONE' and sum(PRICE) >= 700000
+# 내 풀이
 
--- 완료된 중고거래의 총금액이 70만원 이상인 사람
--- STATUS = 'DONE' and sum(PRICE) >= 700000
-with Done_user_higher_700000 as (
-    select b.WRITER_ID, sum(b.PRICE) as TOTAL_SALES
-    from USED_GOODS_BOARD b
-    join USED_GOODS_USER u on b.WRITER_ID = u.USER_ID
-    where b.STATUS = 'DONE'
-    group by b.WRITER_ID
-    having sum(PRICE) >= 700000
-)
-select d.WRITER_ID, u.NICKNAME, d.TOTAL_SALES
-from Done_user_higher_700000 d
-join USED_GOODS_USER u on d.WRITER_ID = u.USER_ID
-order by TOTAL_SALES asc;
+# with Done_user_higher_700000 as (
+#     select b.WRITER_ID, sum(b.PRICE) as TOTAL_SALES
+#     from USED_GOODS_BOARD b
+#     join USED_GOODS_USER u on b.WRITER_ID = u.USER_ID
+#     where b.STATUS = 'DONE'
+#     group by b.WRITER_ID
+#     having sum(PRICE) >= 700000
+# )
+# select d.WRITER_ID, u.NICKNAME, d.TOTAL_SALES
+# from Done_user_higher_700000 d
+# join USED_GOODS_USER u on d.WRITER_ID = u.USER_ID
+# order by TOTAL_SALES asc;
+
+SELECT
+    U.USER_ID,
+    U.NICKNAME,
+    SUM(B.PRICE) AS TOTAL_SALES
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_USER U
+  ON B.WRITER_ID = U.USER_ID
+WHERE B.STATUS = 'DONE'
+GROUP BY U.USER_ID, U.NICKNAME
+HAVING SUM(B.PRICE) >= 700000
+ORDER BY TOTAL_SALES ASC;
