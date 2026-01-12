7a0afcd1b6e51742d90de05a6af21d5534b3c615
주언 <137984851+sulsikan@users.noreply.github.com>
2026-01-12 23:35:52 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.06 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,19 +1,18 @@
 def solution(n, w, num):
-    # 박스를 먼저 쌓는다
+    # 오른쪽 왼쪽 번갈아 가며 2차원 배열에 택배상자를 쌓자
+    # 각 열을 기준으로 배열을 만들어, 상자를 꺼낼 때 해당 열의 인덱스를 계산해서 꺼내자
     boxs = [[] for _ in range(w)]
-    box_num = 0
     direction = 1
-    
-    while box_num < n:
-        for idx in range(0, w, direction) if direction == 1 else range(w-1, -1, -1):
+    box_num = 0
+    # 1 ~ n까지 배열에 담기
+    while box_num <= n:
+        for i in range(0, w, direction) if direction == 1 else range(w-1, -1, -1):
             box_num += 1
-            boxs[idx].append(box_num)
-            if box_num >= n:
+            if box_num > n:
                 break
+            boxs[i].append(box_num)
         direction *= -1
-    
-    for values in boxs:
-        if num in values:
-            answer = len(values) - values.index(num)
-        
+    for row in boxs:
+        if num in row:
+            answer = len(row) - row.index(num)
     return answer
