2c6578ad83cf2797144a50c4610a398d6a4f856b
주언 <137984851+sulsikan@users.noreply.github.com>
2026-01-13 12:35:18 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 0.03 ms
+메모리: 9.22 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 10일 13:27:21
+2026년 01월 13일 12:35:15
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,34 +1,24 @@
-'''
-공항으로 가는 배열
-1. icn이 있는 배열을 찾는다
-2. 목적지를 answer에 추가한다
-3. 목적지의 마지막 값이 있는 배열을 찾는다
-4. 반복한다.
-'''
 from collections import defaultdict
 
 def solution(tickets):
+    # 출발지 : 목적지로 나누어진 딕셔너리 형태로 tickets를 재선언
     graph = defaultdict(list)
-
-    # 1) 그래프 구성
-    for a, b in tickets:
-        graph[a].append(b)
-
-    # 2) 사전순 경로를 위해: pop()으로 가장 작은 목적지가 나오게
-    #    목적지를 내림차순 정렬해두면 pop()은 가장 작은 것부터 꺼냄
+    for fr, to in tickets:
+        graph[fr].append(to)
+    
+    # pop으로 answer에 추가할 것이기 때문에 목적지 알파벳 역순으로 정렬.
     for a in graph:
         graph[a].sort(reverse=True)
-
-    route = []
+        
+    # pop으로 answer에 경로 추가
     stack = ["ICN"]
-
-    # 3) Hierholzer 방식 DFS
+    route = []
+    
     while stack:
-        cur = stack[-1]
-        if graph[cur]:              # 갈 곳이 남아 있으면
-            stack.append(graph[cur].pop())
-        else:                       # 더 이상 갈 곳이 없으면 경로 확정
-            route.append(stack.pop())
-
-    # 4) 역순으로 쌓였으니 뒤집기
+        top = stack[-1]
+        if graph[top]:
+            stack.append(graph[top].pop())  # 가장 사전순 작은 목적지로 이동
+        else:
+            route.append(stack.pop())       # 더 갈 곳 없으면 경로 확정
+            
     return route[::-1]
