f33e0a063d0bf6e1bce41b5fd2baf06c479b1bed
주언 <137984851+sulsikan@users.noreply.github.com>
2026-01-21 10:38:44 +0900

    [Silver II] Title: 한 줄로 서기, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -1,13 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
 taller = list(map(int, input().split()))
-answer = [0] * N
 
-for i in range(N):
+answer = [0] * n
+
+
+for i in range(n):
     cnt = 0
-    for j in range(N):
+    for j in range(n):
         if cnt == taller[i] and answer[j] == 0:
             answer[j] = i+1
             break
