845ea66d280761c24dbd959743abc9f9e072f13e
주언 <137984851+sulsikan@users.noreply.github.com>
2026-02-02 21:40:18 +0900

    [Silver IV] Title: 제로, Time: 64 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -4,10 +4,10 @@ input = sys.stdin.readline
 k = int(input())
 stack = []
 for _ in range(k):
-    num = int(input().strip())
-    if num != 0:
-        stack.append(num)
-    else:
+    num = int(input())
+    if num == 0:
         stack.pop()
+    else:
+        stack.append(num)
 
 print(sum(stack))
