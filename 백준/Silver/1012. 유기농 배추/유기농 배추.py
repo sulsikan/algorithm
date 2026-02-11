cd9036819059897afb2bac49ef46c988410f9d45
주언 <137984851+sulsikan@users.noreply.github.com>
2026-02-12 00:11:08 +0900

    [Silver II] Title: 유기농 배추, Time: 36 ms, Memory: 32728 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,36 +1,42 @@
+# 1012 유기농 배추
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-dy = [0, 1, 0, -1]
-dx = [1,0,-1,0]
-ans =[]
-
-def dfs(x, y):
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if 0 <= nx < M and 0 <= ny < N and board[nx][ny] == True:
-            board[nx][ny] = False
-            dfs(nx, ny)
+def dfs(x, y, visited):
+    visited[x][y] = 1
+    if 0 <= x < M and 0 <= y < N:
+        # 상
+        if 1 <= y < N:
+            if land[x][y-1] == 1 and visited[x][y-1] == 0:
+                dfs(x, y - 1, visited)
+        # 하
+        if 0 <= y < N-1:
+            if land[x][y+1] == 1 and visited[x][y+1] == 0:
+                dfs(x, y + 1, visited)
+        # 좌
+        if 1 <= x < M:
+            if land[x-1][y] == 1 and visited[x-1][y] == 0:
+                dfs(x - 1, y, visited)
+        # 우
+        if 0 <= x < M-1:
+            if land[x+1][y] == 1 and visited[x+1][y] == 0:
+                dfs(x + 1, y, visited)
 
 T = int(input())
 for _ in range(T):
     M, N, K = map(int, input().split())
-    board = [[False] * N for _ in range(M)]
-    # 지도 만들기
+    land = [[0] * N for _ in range(M)]
     for _ in range(K):
-        i, j = map(int, input().split())
-        board[i][j] = True
-    # 실행
+        x, y = map(int, input().split())
+        land[x][y] = 1
+
+    visited = [[0] * N for _ in range(M)]
     cnt = 0
 
     for i in range(M):
         for j in range(N):
-            if board[i][j] == True:
-                dfs(i, j)
+            if land[i][j] == 1 and visited[i][j] == 0:
+                dfs(i, j, visited)
                 cnt += 1
-    ans.append(cnt)
-
-for i in ans:
-    print(i)
+    print(cnt)
