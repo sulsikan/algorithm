3d46a0755064af04ebe966b6767eb9595a9592a0
주언 <137984851+sulsikan@users.noreply.github.com>
2026-03-18 22:49:23 +0900

    [Gold II] Title: 제자리 멀리뛰기, Time: 1496 ms, Memory: 34848 KB -BaekjoonHub

--- 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.py
+++ 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.py
@@ -1,22 +1,19 @@
-import sys
-input = sys.stdin.readline
 
 d, n, m = map(int, input().split())
-rocks = [int(input()) for _ in range(n)] + [d]
+rocks = [int(input()) for _ in range(n)] +[d]
 rocks.sort()
 
 left, right, answer = 0, d, 0
-
 while left <= right:
-    mid = (left + right) // 2
-    prev, cnt = 0, 0            # 현재위치, 바위를 제거한 개수
+    mid = (left+right) // 2
+    prev, remove_cnt = 0, 0
     for rock in rocks:
         if rock - prev < mid:
-            cnt += 1
+            remove_cnt += 1
         else:
             prev = rock
 
-    if cnt <= m:
+    if remove_cnt <= m:
         left = mid + 1
         answer = mid
     else:
