3ab291092eaeef31882043c1c0eaf66c86afd644
주언 <137984851+sulsikan@users.noreply.github.com>
2026-03-19 14:24:37 +0900

    [Gold V] Title: 입국심사, Time: 988 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.py
+++ 백준/Gold/3079. 입국심사/입국심사.py
@@ -1,16 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int,input().split())
 immigration_time = [int(input()) for _ in range(n)]
 
-start, end = 0, min(immigration_time)*m
+start, end = 0, min(immigration_time) * m
 while start <= end:
     mid = (start+end) // 2
-    sum = 0
+    sum_people_count = 0
     for immi_time in immigration_time:
-        sum += mid // immi_time
+        sum_people_count += mid // immi_time  # 처리 가능한 인원 수
 
-    if sum >= m:
-        end = mid - 1
+    if sum_people_count >= m:
         answer = mid
+        end = mid - 1
     else:
         start = mid + 1
 
