4ae569dda24ebfde74e0843862f152b6fcf8a532
주언 <137984851+sulsikan@users.noreply.github.com>
2026-03-20 23:48:47 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,22 +1,12 @@
 n = int(input())
 
-time = [0] * (n+1)
-profit = [0] * (n+1)
+schedule = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(1, n+1):
-    t, p = map(int,input().split())
-    time[i] = t
-    profit[i] = p
+dp = [0 for _ in range(n+1)]
 
-dp = [0] * (n+2)
+for i in range(n):
+    for j in range(i + schedule[i][0], n+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
 
-for i in range(n, 0, -1):
-    if i + time[i] <= n+1:
-        dp[i] = max(
-            dp[i+1],                # 상담 안함
-            profit[i] + dp[i+time[i]]  # 상담 함
-        )
-    else:
-        dp[i] = dp[i+1]
-
-print(dp[1])
+print(dp[-1])
