c74edb51e1cc12da0ee28d19845fc1db20ec8a26
주언 <137984851+sulsikan@users.noreply.github.com>
2026-03-24 23:35:53 +0900

    [Gold III] Title: 파티, Time: 1080 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -2,49 +2,34 @@ import heapq
 import sys
 input = sys.stdin.readline
 
+def dijkstra(s):
+    dist = [1e9] * (N+1)
+    dist[s] = 0
+    queue = []
+    heapq.heappush(queue, (0, s))
+
+    while queue:
+        d, v = heapq.heappop(queue)
+        for nd, nv in graph[v]:
+            if d + nd < dist[nv]:
+                dist[nv] = d + nd
+                heapq.heappush(queue, (dist[nv], nv))
+
+    return dist
+
 N, M, X = map(int, input().split())
 graph = [[] for _ in range(N+1)]
 for _ in range(M):
     start, end, time = map(int, input().split())
     graph[start].append((time, end))
 
-INF = int(1e9)
-go = [INF] * (N+1)
-come = [INF] * (N+1)
-queue = []
-go[X], come[X] = 0, 0
-
-# go
+result = [0]*(N+1)
 for i in range(1, N+1):
+    dist = dijkstra(i)
     if i == X:
+        for j in range(1,N+1):
+            result[j] += dist[j]
         continue
-    heapq.heappush(queue, (0, i))
-    vtox = [INF] * (N+1)
-    vtox[i] = 0
-    while queue:
-        dist, village = heapq.heappop(queue)
-        if village == X and dist < go[i]:
-            go[i] = dist
-            continue
-        for nt, nv in graph[village]:
-            if nt + dist < vtox[nv]:
-                vtox[nv] = nt + dist
-                heapq.heappush(queue, (vtox[nv], nv))
+    result[i] += dist[X]
 
-# come
-heapq.heappush(queue, (0, X))
-
-while queue:
-    dist, village = heapq.heappop(queue)
-    for nt, nv in graph[village]:
-        if nv == X:
-            continue
-        if come[nv] > nt+dist:
-            come[nv] = nt+dist
-            heapq.heappush(queue, (come[nv], nv))
-
-answer = []
-for i in range(1, N+1):
-    answer.append(go[i]+come[i])
-
-print(max(answer))
+print(max(result))
