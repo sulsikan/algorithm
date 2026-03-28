b61eed5c52076d088369dc9889d1f80a47861f04
주언 <137984851+sulsikan@users.noreply.github.com>
2026-03-28 12:54:15 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,2 +1,24 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-print("SK" if n % 2 == 1 else "CY")
+dp = [False] * (n+1)
+
+
+if n == 1:
+    print("SK")
+    exit()
+elif n == 2:
+    print("CY")
+    exit()
+
+dp[1] = True
+dp[2] = False
+dp[3] = True
+
+for i in range(4, n+1):
+    # 1개 또는 3개 빼서 상대를 지는 상태로 만들 수 있으면 이김
+    if not dp[i-1] or not dp[i-3]:
+        dp[i] = True
+
+print("SK" if dp[n] else "CY")
