e4358f95178d5ff1268b65f8fbac37a00430b88f
주언 <137984851+sulsikan@users.noreply.github.com>
2026-04-02 11:12:44 +0900

    [Gold IV] Title: 동전 1, Time: 192 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coin_arr = [int(input()) for _ in range(n)]
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for coin in coin_arr:
+    for i in range(coin, k+1):
+        dp[i] += dp[i-coin]
+
+print(dp[k])
