bdda73f508e2d1172b9577a304f1d073fc97b3bb
주언 <137984851+sulsikan@users.noreply.github.com>
2026-04-03 22:40:14 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -1,21 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-# input
-n, k = map(int, input().split())
-medals = [list(map(int, input().split())) for _ in range(n)]
+N, K = map(int, input().split())
+medal_state = [list(map(int, input().split())) for _ in range(N)]
 
-# 정렬
-medals.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
+medal_state.sort(key=lambda x : (x[1], x[2], x[3]), reverse=True)
 
-# k 점수 찾기
-for nation, g, s, b in medals:
-    if nation == k:
-        k_score = (g, s, b)
+rank, tmp = 1, 0
+for i in range(1, N + 1):
+    if medal_state[i-1][0] == K:
+        print(rank)
         break
-
-# k_score가 처음 나오는 위치가 순위
-for i, (nation, g, s, b) in enumerate(medals):
-    if (g, s, b) == k_score:
-        print(i + 1)
-        break
+    if medal_state[i-1][1:] != medal_state[i][1:]:
+        rank += (1 + tmp)
+        tmp = 0
+    else:
+        tmp += 1
