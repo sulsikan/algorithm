3f35881e67adf46a28776943fbe0dfc0672c9d31
주언 <137984851+sulsikan@users.noreply.github.com>
2026-05-08 11:10:32 +0900

    [level 2] Title: 배달, Time: 1.21 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,43 +1,36 @@
 import heapq
 
 def solution(N, road, K):
-
+    answer = 0
     INF = int(1e9)
-
+    
+    # create a graph
     graph = [[] for _ in range(N+1)]
-
     for a, b, c in road:
-
         graph[a].append((b, c))
         graph[b].append((a, c))
-
+        
+    # initialize data structure
     distance = [INF] * (N+1)
-
     distance[1] = 0
-
     pq = []
-
     heapq.heappush(pq, (0, 1))
-
+    
+    # shortest distance calculation
     while pq:
-        dist, now = heapq.heappop(pq)
-
-        if distance[now] < dist:
+        cost, node = heapq.heappop(pq)
+        
+        if distance[node] < cost:
             continue
-
-        for next_node, cost in graph[now]:
-            new_cost = dist + cost
-            if new_cost < distance[next_node]:
-                distance[next_node] = new_cost
-                heapq.heappush(
-                    pq,
-                    (new_cost, next_node)
-                )
-
-    answer = 0
-
+            
+        for next_node, next_cost in graph[node]:
+            next_cost += cost
+            if next_cost < distance[next_node]:
+                distance[next_node] = next_cost
+                heapq.heappush(pq, (next_cost, next_node))
+            
     for d in distance:
         if d <= K:
             answer += 1
-
+    
     return answer
