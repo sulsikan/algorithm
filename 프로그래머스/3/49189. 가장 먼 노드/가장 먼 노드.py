77a4b8551fbb2e5f039c102d4315ee321dd84126
주언 <137984851+sulsikan@users.noreply.github.com>
2026-05-26 22:04:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 3634.46 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -8,13 +8,12 @@ def solution(n, edge):
     for a, b in edge:
         graph[a].append(b)
         graph[b].append(a)
-    
+        
     # initialize data structure
+    queue = deque([1])
     dist = [-1] * (n+1)
     dist[1] = 0
-    queue = deque([1])
     
-    # play bfs
     while queue:
         now = queue.popleft()
         
