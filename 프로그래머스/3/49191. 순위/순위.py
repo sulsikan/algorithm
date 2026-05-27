9af3505f2d05454748a9377b990d2348732c949d
주언 <137984851+sulsikan@users.noreply.github.com>
2026-05-28 06:39:54 +0900

    [level 3] Title: 순위, Time: 63.94 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,32 +1,28 @@
+# 플루이드워셜
+
 def solution(n, results):
-
-    graph = [[False] * (n + 1) for _ in range(n + 1)]
-
-    # 승패 기록
-    for winner, loser in results:
-        graph[winner][loser] = True
-
-    # 플로이드 워셜
-    for k in range(1, n + 1):
-        for a in range(1, n + 1):
-            for b in range(1, n + 1):
-
-                # a가 k를 이기고
-                # k가 b를 이기면
-                # a가 b를 이김
-                if graph[a][k] and graph[k][b]:
-                    graph[a][b] = True
-
+    win_result_table = [[False] * (n+1) for _ in range(n+1)]
+    lose_result_table = [[False] * (n+1) for _ in range(n+1)]
+    
+    for win, lose in results:
+        win_result_table[win][lose] = True
+        lose_result_table[lose][win] = True
+        
+    for k in range(n+1):
+        for i in range(n+1):
+            for j in range(n+1):
+                if win_result_table[i][k] and win_result_table[k][j]:
+                    win_result_table[i][j] = True
+                if lose_result_table[i][k] and lose_result_table[k][j]:
+                    lose_result_table[i][j] = True
+                    
     answer = 0
-
-    for i in range(1, n + 1):
-        count = 0
-
-        for j in range(1, n + 1):
-            if graph[i][j] or graph[j][i]:
-                count += 1
-
-        if count == n - 1:
+    
+    for i in range(1, n+1):
+        win_cnt = win_result_table[i].count(True)
+        lose_cnt = lose_result_table[i].count(True)
+        
+        if win_cnt + lose_cnt == n-1:
             answer += 1
 
     return answer
