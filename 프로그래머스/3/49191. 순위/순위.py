ee7f221cf58bb6051b1d18a02f1d86c45698daab
주언 <137984851+sulsikan@users.noreply.github.com>
2026-05-27 17:24:19 +0900

    [level 3] Title: 순위, Time: 33.66 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,37 +1,32 @@
-# bfs
-from collections import deque
 def solution(n, results):
-    answer = 0
-    winner_graph = [[] for _ in range(n+1)]
-    loser_graph = [[] for _ in range(n+1)]
-    
-    for w, l in results:
-        winner_graph[w].append(l)
-        loser_graph[l].append(w)
-        
-    def bfs(graph, start):
-        visited = [False] * (n+1)
-        queue = deque([start])
-        count = 0
-        
-        visited[start] = True
-    
-        while queue:
-            player = queue.popleft()
 
-            for nxt in graph[player]:
-                if not visited[nxt]:
-                    visited[nxt] = True
-                    queue.append(nxt)
-                    count += 1
-        return count
-    
-    for p in range(1, n+1):
-        win_count = bfs(winner_graph, p)
-        
-        lose_count = bfs(loser_graph, p)
-        
-        if win_count + lose_count == n-1:
+    graph = [[False] * (n + 1) for _ in range(n + 1)]
+
+    # 승패 기록
+    for winner, loser in results:
+        graph[winner][loser] = True
+
+    # 플로이드 워셜
+    for k in range(1, n + 1):
+        for a in range(1, n + 1):
+            for b in range(1, n + 1):
+
+                # a가 k를 이기고
+                # k가 b를 이기면
+                # a가 b를 이김
+                if graph[a][k] and graph[k][b]:
+                    graph[a][b] = True
+
+    answer = 0
+
+    for i in range(1, n + 1):
+        count = 0
+
+        for j in range(1, n + 1):
+            if graph[i][j] or graph[j][i]:
+                count += 1
+
+        if count == n - 1:
             answer += 1
-    
+
     return answer
