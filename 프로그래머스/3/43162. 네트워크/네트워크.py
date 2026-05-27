454ea1a77b02d9e9e7361a122decf55887bb57f6
주언 <137984851+sulsikan@users.noreply.github.com>
2026-05-27 15:38:29 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,23 +1,29 @@
-from collections import deque
-# 노드간선그래프-> dfs/bfs -> 다른 하나라도 이어져있으면 T 아니면 F
 def solution(n, computers):
-    answer = 0
-    visited = [False for _ in range(n)]
+    graph = [[] for _ in range(n)]
+    
     for i in range(n):
-        if visited[i] == False:
-            BFS(n, computers, i, visited)
-            answer += 1
-    return answer
-
-def BFS(n, computers, i, visited):
-    queue = deque()
-    queue.append(i)
-    while queue:
-        com = queue.popleft()
-        visited[com] = True
-        for connect in range(n):
-            if visited[connect]==False and computers[com][connect] == 1:
-                queue.append(connect)
+        for j in range(n):
+            if i != j and computers[i][j] == 1:
+                graph[i].append(j)
                 
-        
+    stack = [0]
+    visited = [False] * n 
+    answer = 0
+    
+    for node in range(n):
+        if visited[node] == False:
+            stack.append(node)
+        else:
+            continue
             
+        while stack:
+            now = stack.pop()
+            for nxt in graph[now]:
+                if visited[nxt] == False:
+                    visited[nxt] = True
+                    stack.append(nxt)
+
+        answer += 1
+    
+    
+    return answer
