d1ab2b73bfe2b682155810aae6513e91fd11aadd
주언 <137984851+sulsikan@users.noreply.github.com>
2026-05-28 15:13:52 +0900

    [level 2] Title: 타겟 넘버, Time: 124.57 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,19 @@
-# 전 풀이 참고
-# 노드 엣지 구성의 그래프로 한번 생각해봐라
-# 원하는 타겟이 나올 때 까지 탐색하는 dfs
-answer = 0
-def solution(numbers, target):
-    dfs(0, 0, numbers, target)
-    return answer
+# 모든 경우의 수 -> target인 것만 count
 
-def dfs(idx, total, numbers, target):
-    global answer
-    if idx == len(numbers):
-        if total == target:
-            answer += 1
-        return
-    dfs(idx+1, total+numbers[idx], numbers, target)
-    dfs(idx+1, total-numbers[idx], numbers, target)
+def solution(numbers, target):
+    answer = 0
+
+    def dfs(idx, total):
+        nonlocal answer
+
+        if idx == len(numbers):
+            if total == target:
+                answer += 1
+            return
+
+        dfs(idx + 1, total + numbers[idx])
+        dfs(idx + 1, total - numbers[idx])
+
+    dfs(0, 0)
+
+    return answer
