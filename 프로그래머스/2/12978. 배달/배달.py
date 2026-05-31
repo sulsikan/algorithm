be2b507209c0c6b0832e46ba30a6b34287af6987
주언 <137984851+sulsikan@users.noreply.github.com>
2026-06-01 05:26:41 +0900

    [level 2] Title: 배달, Time: 1.36 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,36 +1,32 @@
+# 다익스트라 최단경로 모든 마을 구하기
+# 조건 확인해서 answer 카운트
+
 import heapq
 
 def solution(N, road, K):
-    answer = 0
-    INF = int(1e9)
+    INF = 1e9
     
-    # create a graph
     graph = [[] for _ in range(N+1)]
     for a, b, c in road:
         graph[a].append((b, c))
         graph[b].append((a, c))
         
-    # initialize data structure
+    queue = [(0, 1)]
     distance = [INF] * (N+1)
     distance[1] = 0
-    pq = []
-    heapq.heappush(pq, (0, 1))
     
-    # shortest distance calculation
-    while pq:
-        cost, node = heapq.heappop(pq)
+    while queue:
+        cur_cost, cur_node = heapq.heappop(queue)
         
-        if distance[node] < cost:
-            continue
-            
-        for next_node, next_cost in graph[node]:
-            next_cost += cost
-            if next_cost < distance[next_node]:
-                distance[next_node] = next_cost
-                heapq.heappush(pq, (next_cost, next_node))
-            
+        for nxt_node, cost in graph[cur_node]:
+            nxt_cost = cost + cur_cost
+            if distance[nxt_node] > nxt_cost:
+                distance[nxt_node] = nxt_cost
+                heapq.heappush(queue, (nxt_cost, nxt_node))
+    
+    answer = 0
     for d in distance:
         if d <= K:
             answer += 1
-    
+            
     return answer
