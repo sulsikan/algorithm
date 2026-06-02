1a13d0e693486c83b34ee677b97ac92c8977c826
주언 <137984851+sulsikan@users.noreply.github.com>
2026-06-03 00:24:33 +0900

    [level 2] Title: 주차 요금 계산, Time: 5.79 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,39 @@
+from collections import defaultdict, deque
+import math
+
+def solution(fees, records):
+    answer = []
+    calc_list = dict()
+    accrue_car = defaultdict(int)
+    queue = []
+    
+    # 차량 별 누적시간 기록
+    # 시간, 차량번호, 상태 / 시간 계산이 관건
+    for row in records:
+        t, n, s = row.split()
+        min_t = int(t[0:2]) * 60 + int(t[3:])
+
+        if s == 'IN':
+            calc_list[n] = min_t
+            if n not in queue:
+                queue.append(n)
+                
+        else:
+            accrue_car[n] += min_t - calc_list[n]
+            calc_list[n] = -1
+        
+    # 가격표 계산
+    queue.sort()
+    
+    for car_num in queue:
+
+        if calc_list[car_num] >= 0:
+            accrue_car[car_num] += (23 * 60 + 59) - calc_list[car_num]
+            
+        if accrue_car[car_num] < fees[0]:
+            answer.append(fees[1])
+            continue
+        
+        answer.append(fees[1] + math.ceil((accrue_car[car_num] - fees[0]) / fees[2]) * fees[3])
+
+    return answer
