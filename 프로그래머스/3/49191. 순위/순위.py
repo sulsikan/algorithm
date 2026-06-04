3009c95f7f8c9a29153479a357f14a9a391027bd
주언 <137984851+sulsikan@users.noreply.github.com>
2026-06-04 14:17:54 +0900

    [level 3] Title: 순위, Time: 39.86 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,28 +1,25 @@
-# 플루이드워셜
-
 def solution(n, results):
-    win_result_table = [[False] * (n+1) for _ in range(n+1)]
-    lose_result_table = [[False] * (n+1) for _ in range(n+1)]
-    
-    for win, lose in results:
-        win_result_table[win][lose] = True
-        lose_result_table[lose][win] = True
-        
-    for k in range(n+1):
-        for i in range(n+1):
-            for j in range(n+1):
-                if win_result_table[i][k] and win_result_table[k][j]:
-                    win_result_table[i][j] = True
-                if lose_result_table[i][k] and lose_result_table[k][j]:
-                    lose_result_table[i][j] = True
-                    
     answer = 0
-    
-    for i in range(1, n+1):
-        win_cnt = win_result_table[i].count(True)
-        lose_cnt = lose_result_table[i].count(True)
-        
-        if win_cnt + lose_cnt == n-1:
-            answer += 1
 
+    players = [[0] * (n+1) for _ in range(n+1)]
+    
+    for winner, loser in results:
+        players[winner][loser] = 1
+    
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if players[i][k] == 1 and players[k][j] == 1:
+                    players[i][j] = 1
+
+    for i in range(1, n + 1):
+        count = 0
+
+        for j in range(1, n + 1):
+            if players[i][j] or players[j][i]:
+                count += 1
+
+        if count == n - 1:
+            answer += 1
+        
     return answer
