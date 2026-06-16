eabd626c51cc98641b7bbc6e4bf487ff80f5010e
주언 <137984851+sulsikan@users.noreply.github.com>
2026-06-16 12:28:33 +0900

    [level 3] Title: 순위, Time: 36.69 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,25 +1,27 @@
 def solution(n, results):
-    answer = 0
-
-    players = [[0] * (n+1) for _ in range(n+1)]
+    winners = [[] for _ in range(n+1)]
     
-    for winner, loser in results:
-        players[winner][loser] = 1
+    for a, b in results:
+        winners[a].append(b)
+        
+    graph = [[False] * (n+1) for _ in range(n+1)]
+    
+    for a, b in results:
+        graph[a][b] = True
     
     for k in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
-                if players[i][k] == 1 and players[k][j] == 1:
-                    players[i][j] = 1
-
-    for i in range(1, n + 1):
+                if graph[i][k] and graph[k][j]:
+                    graph[i][j] = True
+    answer = 0         
+    for i in range(1, n+1):
         count = 0
-
-        for j in range(1, n + 1):
-            if players[i][j] or players[j][i]:
+        for j in range(1, n+1):
+            if graph[i][j] or graph[j][i]:
                 count += 1
-
-        if count == n - 1:
+                
+        if count == n-1:
             answer += 1
-        
+                
     return answer
