2357df27dc05fa30fe893f2b68854689c929a09d
주언 <137984851+sulsikan@users.noreply.github.com>
2026-06-19 22:39:18 +0900

    [level 3] Title: N으로 표현, Time: 6.65 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -3,12 +3,10 @@ def solution(N, number):
     
     if N == number:
         return 1
-    
-    for k in range(1,9):
-        # 1) NNN...N (붙이기) 추가
-        dp[k].add(int(str(N)*k))
+
+    for k in range(1, 9):
+        dp[k].add(int(str(N) * k))
         
-        # 2) 두 집합 조합 (i + (k-i))
         for i in range(1, k):
             for a in dp[i]:
                 for b in dp[k-i]:
@@ -16,11 +14,10 @@ def solution(N, number):
                     dp[k].add(a - b)
                     dp[k].add(a * b)
                     if b != 0:
-                        dp[k].add(a//b)
-                        
+                        dp[k].add(a // b)
+        
         if number in dp[k]:
             return k
         
     return -1
-
-
+                
