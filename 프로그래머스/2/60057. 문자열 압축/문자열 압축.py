52361787c6f110179f3af8f27d7e94fcd2b33c34
주언 <137984851+sulsikan@users.noreply.github.com>
2026-07-15 23:44:36 +0900

    [level 2] Title: 문자열 압축, Time: 1.13 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,29 +1,29 @@
 def solution(s):
     answer = len(s)
     
-    for num in range(1, len(s)//2 + 1):
+    for num in range(1, len(s)//2+1):
         result = ''
-        count = 1
-        
+        cnt = 1
         now = s[:num]
         
         for i in range(num, len(s), num):
             nxt = s[i:i + num]
             
-            if nxt == now:
-                count += 1
+            if now == nxt:
+                cnt += 1
             else:
-                if count == 1:
+                if cnt == 1:
                     result += now
                 else:
-                    result += str(count) + now
+                    result += str(cnt) + now
                 now = nxt
-                count = 1
+                cnt = 1
                 
-        if count > 1:
-            result += str(count) + now
+        if cnt > 1:
+            result += str(cnt) + now
         else:
             result += now
+            
         answer = min(answer, len(result))
         result = ''
         
