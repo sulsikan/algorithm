5cf934b5177b354c552b454f4fe93e16b862e9c9
주언 <137984851+sulsikan@users.noreply.github.com>
2026-07-16 15:48:42 +0900

    [level 2] Title: 방문 길이, Time: 1.06 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,25 +1,33 @@
 def solution(dirs):
-    visited = set()
-    x, y = 0, 0
+    answer = 0
+    path = [[] for _ in range(len(dirs)+1)]
+    path[0] = [5, 5]
+    start = [5, 5]
+    visited = []
     
-    move = {
-        "U" : (0, 1),
-        "D" : (0, -1),
-        "L" : (-1, 0),
-        "R" : (1, 0)
-    }
+    for i in range(0, len(dirs)):
+        
+        if dirs[i] == 'U':
+            path[i+1] = [path[i][0], path[i][1]+1]       
+        elif dirs[i] == 'D':
+            path[i+1] = [path[i][0], path[i][1]-1]
+        elif dirs[i] == 'R':
+            path[i+1] = [path[i][0]+1, path[i][1]]
+        else:
+            path[i+1] = [path[i][0]-1, path[i][1]]
+            
+        if 0 <= path[i+1][0] < 11 and 0 <= path[i+1][1] < 11:
+            chk_p = [start[0], start[1], path[i+1][0], path[i+1][1]]
+            
+            if chk_p not in visited:
+                print(chk_p)
+                visited.append(chk_p)
+                visited.append([path[i+1][0], path[i+1][1], start[0], start[1]])
+                answer += 1
+        
+            start = [path[i+1][0], path[i+1][1]]
+            
+        else:
+            path[i+1] = path[i]
     
-    for d in dirs:
-        dx, dy = move[d]
-        nx, ny = x + dx, y + dy
-        
-        if nx < -5 or nx > 5 or ny < -5 or ny > 5:
-            continue
-
-        visited.add((x, y, nx, ny))
-        visited.add((nx, ny, x, y))
-        
-        x, y = nx, ny
-    
-    return len(visited) // 2
-        
+    return answer
